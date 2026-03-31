0a3a6fbb3865139efbb4d4dca18bd5e3df2956e5
goshan <goshan.hanqiu@gmail.com>
2026-03-31 15:35:39 +0000

    fixed backup

--- bins/backup.sh
+++ bins/backup.sh
@@ -15,28 +15,28 @@ mkdir -p $BACKUP_DIR
 skill_backup="$BACKUP_DIR/skills_$TIME.bak"
 cp -r "$OPENCLAW_SKILL/" "$skill_backup"
 # only keep the latest 3 backups
-printf '%s\n' "$BACKUP_DIR/skills_*.bak" | sort -r | tail -n +4 | xargs -I {} rm -rf -- "{}"
+printf '%s\n' "$BACKUP_DIR"/skills_*.bak | sort -r | tail -n +4 | xargs -I {} rm -rf -- "{}"
 echo "Backup skills to $skill_backup"
 echo ""
 
 crontab_backup="$BACKUP_DIR/crontab_$TIME.bak"
 crontab -l > $crontab_backup
 # only keep the latest 3 backups
-printf '%s\n' "$BACKUP_DIR/crontab_*.bak" | sort -r | tail -n +4 | xargs -I {} rm -- "{}"
+printf '%s\n' "$BACKUP_DIR"/crontab_*.bak | sort -r | tail -n +4 | xargs -I {} rm -- "{}"
 echo "Backup crontab to $crontab_backup"
 echo ""
 
 openclaw_cron_backup="$BACKUP_DIR/openclaw_jobs_$TIME.bak.json"
 cp $OPENCLAW_CRON $openclaw_cron_backup
 # only keep the latest 3 backups
-printf '%s\n' "$BACKUP_DIR/openclaw_jobs_*.bak.json" | sort -r | tail -n +4 | xargs -I {} rm -- "{}"
+printf '%s\n' "$BACKUP_DIR"/openclaw_jobs_*.bak.json | sort -r | tail -n +4 | xargs -I {} rm -- "{}"
 echo "Backup openclaw cron jobs to $openclaw_cron_backup"
 echo ""
 
 db_backup="$BACKUP_DIR/data_$TIME.bak"
 cp -r "$HOME_DIR/data/" "$db_backup"
 # only keep the latest 3 backups
-printf '%s\n' "$BACKUP_DIR/data_*.bak" | sort -r | tail -n +4 | xargs -I {} rm -rf -- "{}"
+printf '%s\n' "$BACKUP_DIR"/data_*.bak | sort -r | tail -n +4 | xargs -I {} rm -rf -- "{}"
 echo "Backup database to $db_backup"
 echo ""
 
